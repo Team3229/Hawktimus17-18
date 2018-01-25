9d400dae9fbd605c6d874b1d2c9c04bc8527ce51
haydenmann3 <35817216+haydenmann3@users.noreply.github.com>
2018-01-25 16:11:22 -0500

    Uploaded the code for the 2018 robot.  ALL CODE FOR THE NEW ROBOT SHOULD BE PLACED HERE.

---
+++ Robot2018/src/Climber.cpp
@@ -0,0 +1,23 @@
+#include <Climber.h>
+
+Climber::Climber()
+{
+	myClimber = new frc::Spark(CLIMBER_PWM);
+}
+
+void Climber::Stop()
+{
+	myClimber->StopMotor();
+}
+
+void Climber::Climb()
+{
+	//Climb
+	std::cout << "Climb()" << std::endl;
+	myClimber->Set(CLIMBER_POWER);
+}
+
+void Climber::safetyClimber()
+{
+
+}
