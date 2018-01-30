03c629dad3a39917dbfedbf72061587e1da39e36
lukesnc <35816797+lukesnc@users.noreply.github.com>
2018-01-30 15:45:11 -0500

    CAMERA CODE FROM 2017 ROBOT

---
+++ Robot2018/src/Camera.cpp
@@ -0,0 +1,51 @@
+#include <Camera.h>
+using namespace frc;
+
+Servo * panServo;
+Servo * tiltServo;
+
+Camera::Camera()
+{
+	panServo = new Servo(PAN_SERVO_PWM);
+	tiltServo = new Servo(TILT_SERVO_PWM);
+	cs::AxisCamera camera = CameraServer::GetInstance()->AddAxisCamera("axis-camera.local");
+
+	// Set the camera settings
+	CameraServer::GetInstance()->SetSize(SIZE);
+	camera.SetFPS(FPS);
+	camera.SetWhiteBalanceAuto();
+	camera.SetExposureAuto();
+	camera.SetBrightness(BRIGHTNESS);
+	CameraServer::GetInstance()->StartAutomaticCapture();
+}
+
+void Camera::Reset()
+{
+	std::cout << "Camera Reset()" << std::endl;
+
+	panServo->Set(INITIAL_PAN);
+	tiltServo->Set(INITIAL_TILT);
+}
+
+void Camera::Move (double tilt, double pan)
+{
+	std::cout << "Camera Move()" << std::endl;
+
+	// Move toward desired position
+	if (pan > 0)
+	{
+		panServo->Set(panServo->Get() + PANTILT_RATE);
+	}
+	else
+	{
+		panServo->Set(panServo->Get() - PANTILT_RATE);
+	}
+	if (tilt < 0) // tilt is reversed on Xbox controller
+	{
+		tiltServo->Set(tiltServo->Get() + PANTILT_RATE);
+	}
+	else
+	{
+		tiltServo->Set(tiltServo->Get() - PANTILT_RATE);
+	}
+}
---
+++ Robot2018/src/Camera.h
@@ -0,0 +1,37 @@
+#ifndef SRC_CAMERA_H_
+#define SRC_CAMERA_H_
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+// Constants for Camera Settings
+#define SIZE 0 // 640 x 480
+#define FPS 12
+#define BRIGHTNESS 40
+
+// Constants for Pan/Tilt Servos
+#define PAN_SERVO_PWM 3
+#define TILT_SERVO_PWM 4
+#define INITIAL_PAN 0.2
+#define INITIAL_TILT 0.5
+#define PANTILT_RATE 0.05
+
+#include <GenericHID.h>
+#include <math.h>
+#include <Servo.h>
+#include <CameraServer.h>
+
+class Camera
+{
+public:
+	Camera();
+	void Reset();
+	void Move(double Y, double X);
+
+private:
+	frc::Servo * panServo;
+	frc::Servo * tiltServo;
+};
+
+#endif /* SRC_CAMERA_H_ */
