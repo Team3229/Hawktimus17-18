5dcfe0b5fa31132ef60f27ef5119f7121d4282fe
haydenmann3 <35817216+haydenmann3@users.noreply.github.com>
2018-02-08 15:02:49 -0500

    Add files via upload

--- Robot2018/src/Autonomous.cpp
+++ Robot2018/src/Autonomous.cpp
@@ -1,34 +1,67 @@
-#include <Autonomous.h>
+#include "Autonomous.h"
 
 Autonomous::Autonomous()
 {
 	gyro = new ADXRS450_Gyro();
-	chooser = new frc::SendableChooser<int>();
 	ultra = new AnalogInput(ULTRA_PORT);
+	positionChooser = new frc::SendableChooser<int*>();
+	targetChooser = new frc::SendableChooser<int*>();
+	delayChooser = new frc::SendableChooser<int*>();
 }
 
 Autonomous::~Autonomous()
 {
 	delete gyro;
-	delete chooser;
 	delete ultra;
+	delete positionChooser;
+	delete targetChooser;
+	delete delayChooser;
 }
 
 
 void Autonomous::AutoInit(std::string colors)
 {
 	switchColor = colors[0]; //Get the color of the switch
-
+	AddOptions();
 	ReadStation();
 }
 
-
-void Autonomous::ReadStation()
-{
-	//Read from smart dashboard, set position, target, and delay.
-}
-
-void Autonomous::AutoPeriodic() { /*Runs either exchange, switch, or baseline based on enums*/}
+void Autonomous::AutoPeriodic(DriveSystem& chasis) { /*Runs either exchange, switch, or baseline based on enums*/}
 void Autonomous::Exchange() {}
 void Autonomous::Switch () {}
 void Autonomous::Baseline() {}
+
+
+void Autonomous::AddOptions()
+{
+	//Function called in constructor to set up dashboard options
+	int Center = 0; //3 variables for 3 options from chooser
+	int Left = 1;
+	int Right = 2;
+	positionChooser->AddDefault("Center", &Center); //Center will be selected by default
+	positionChooser->AddObject("Left", &Left); //Other 2 are other options under it
+	positionChooser->AddObject("Right", &Right);
+	frc::SmartDashboard::PutData("Starting Position", positionChooser); //Labels the dropdown box.
+
+	int Default = 0; //Default option that will have the robot do NOTHING
+	int Exchange = 1; //5 variables for target chooser
+	int Switch = 2;
+	int Scale = 3;
+	int Baseline = 4;
+	targetChooser->AddDefault("Default", &Default); //Default option
+	targetChooser->AddObject("Exchange", &Exchange); //Adds the other 4 target options to the chooser
+	targetChooser->AddObject("Switch", &Switch);
+	targetChooser->AddObject("Scale", &Scale);
+	targetChooser->AddObject("Baseline", &Baseline);
+	frc::SmartDashboard::PutData("Target", targetChooser); //Labels the dropdown box.
+
+	int No = 0; //Default is no, the 2 options for making the robot wait
+	int Yes = 1;
+	delayChooser->AddDefault("No", &No);
+	delayChooser->AddObject("Yes", &Yes);
+	frc::SmartDashboard::PutData("Delay?", delayChooser); //Labels the dropdown box.
+}
+void Autonomous::ReadStation()
+{
+
+}
--- Robot2018/src/Autonomous.h
+++ Robot2018/src/Autonomous.h
@@ -1,3 +1,12 @@
+/*
+ * File:		Autonomous.h
+ * Author:		Programming Subteam
+ * Team:		Hawktimus Prime - 3229
+ *
+ * File Description:
+ * Defines the class for autonomous mode that will be instantiated to drive the robot without a human interface.
+ */
+
 #ifndef SRC_AUTONOMOUS_H
 #define SRC_AUTONOMOUS_H
 
@@ -10,6 +19,7 @@
 #include <ADXRS450_Gyro.h>
 #include <Timer.h>
 #include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
 #include <AnalogInput.h>
 
 //Programmer-Defined Includes
@@ -27,13 +37,17 @@ private:
 	const int ULTRA_PORT = 2;
 
 	ADXRS450_Gyro * gyro; //Instantiates gyro
-	frc::SendableChooser<int> * chooser; //Receiving from the smart dashboard
 	Timer movementTimer(); //For tracking movements.
 	AnalogInput * ultra; //Gets input from distance sensor on AnalogPort
 
+	//Choosing bois
+	frc::SendableChooser<int*> * positionChooser; //Receiving from the smart dashboard
+	frc::SendableChooser<int*> * targetChooser;
+	frc::SendableChooser<int*> * delayChooser;
+
 	//Enums from dashboard
 	enum Position {C, L, R}; //For determining which station we are at
-	enum Target {E, S, B, D}; //For determining which action we want to take.
+	enum Target {D, E, SW, SC, B}; //For determining which action we want to take.
 	enum Delay {Yes, No}; //For determining if delay
 
 	//Initializations of enums
@@ -48,10 +62,30 @@ public:
 	~Autonomous();
 	void AutoInit(std::string colors);
 	void ReadStation();
-	void AutoPeriodic();
+	void AutoPeriodic(DriveSystem& chasis);
 	void Exchange();
 	void Switch ();
 	void Baseline();
+	void AddOptions();
 };
 
 #endif /*SRC_AUTONOMOUS_H*/
+
+
+//Gyro stuff
+		/* gyro->SetSensitivity(GYRO_GAIN);
+		gyro->Reset();
+
+		//ultrasonic stuff
+		ultra = new Ultrasonic(1, 1); // assigns ultra to be an ultrasonic sensor which uses DigitalOutput 1 for the echo pulse and DigitalInput 1 for the trigger pulse
+		ultra->SetEnabled(true);
+		ultra->SetAutomaticMode(true); // turns on automatic mode */
+
+
+//Ultrasonic output WORKING
+		/* double currentDistance = ultra.GetValue() * ValueToInches;
+		std::cout << "Ultrasonic distance: " << currentDistance << std::endl; */
+
+		//Gyro testing area WORKING
+		/* int currentAngle = gyro->GetAngle();
+		std::cout << "Gyro reading: " << currentAngle << std::endl; */
