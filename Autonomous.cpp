116ae1ad744a3231ee1a54aaece63937e25cf0f8
nanderson48 <34905681+nanderson48@users.noreply.github.com>
2018-02-10 11:35:16 -0500

    RESTART ECLIPSE TO UNBREAK THINGS

---
+++ Autonomous.cpp
@@ -0,0 +1,100 @@
+/*
+ * File:		Autonomous.cpp
+ * Author:		Programming Subteam
+ * Team:		Hawktimus Prime - 3229
+ *
+ * File Description:
+ * Contains the code to be executed for autonomous mode.
+ */
+
+#include "Autonomous.h"
+
+Autonomous::Autonomous()
+{
+	gyro = new ADXRS450_Gyro(); //Green gyro
+	ultra = new AnalogInput(ULTRA_PORT);
+	positionChooser = new frc::SendableChooser<int*>();
+	targetChooser = new frc::SendableChooser<int*>();
+	delayChooser = new frc::SendableChooser<int*>();
+
+	AddOptions();
+}
+
+Autonomous::~Autonomous()
+{
+	delete gyro;
+	delete ultra;
+	delete positionChooser;
+	delete targetChooser;
+	delete delayChooser;
+}
+
+
+void Autonomous::AutoInit(std::string colors)
+{
+	switchColor = colors[0]; //Get the color of the switch
+
+	//ReadStation();
+}
+
+void Autonomous::AutoPeriodic(DriveSystem& chasis) { /*Runs either exchange, switch, or baseline based on enums*/}
+void Autonomous::Exchange() {}
+void Autonomous::Switch () {}
+void Autonomous::Baseline() {}
+
+
+void Autonomous::AddOptions()
+{
+	//Function called in constructor to set up dashboard options
+	int Center = 0; //3 variables for 3 options from chooser
+	int Left = 1;
+	int Right = 2;
+	positionChooser->AddDefault("Center", &Center); //Center will be selected by default
+	positionChooser->AddObject("Left", &Left); //Other 2 are other options under it
+	positionChooser->AddObject("Right", &Right);
+	frc::SmartDashboard::PutData("Starting Position", positionChooser); //Labels the dropdown box.
+
+	int Default = 0; //Default option that will have the robot do NOTHING
+	int Exchange = 1; //5 variables for target chooser
+	int Switch = 2;
+	int Scale = 3;
+	int Baseline = 4;
+	targetChooser->AddDefault("Default", &Default); //Default option
+	targetChooser->AddObject("Exchange", &Exchange); //Adds the other 4 target options to the chooser
+	targetChooser->AddObject("Switch", &Switch);
+	targetChooser->AddObject("Scale", &Scale);
+	targetChooser->AddObject("Baseline", &Baseline);
+	frc::SmartDashboard::PutData("Target", targetChooser); //Labels the dropdown box.
+
+	int No = 0; //Default is no, the 2 options for making the robot wait
+	int Yes = 1;
+	delayChooser->AddDefault("No", &No);
+	delayChooser->AddObject("Yes", &Yes);
+	frc::SmartDashboard::PutData("Delay?", delayChooser); //Labels the dropdown box.
+}
+/* void Autonomous::ReadStation()
+{
+	//Check selection of starting station;
+	switch(*(targetChooser->GetSelected()))
+	{
+	case 0: //User specifies Default
+		start = Target::D;
+		break;
+
+	case 1: //User specifices they want the exchange
+		 start = Position::E;
+		 break;
+
+	case 2:
+		start = Target::SW;
+		break;
+
+	case 3:
+		start = Target::SC;
+		break;
+
+	case 4:
+		start = Target::B;
+		break;
+	}
+} */
---
+++ DriveSystem.cpp
@@ -0,0 +1,112 @@
+/*
+ * File:			ArcadeDrive.cpp
+ * Author:			Hayden Mann
+ * Last Modified:	02/03/18
+ * Team:			Hawktimus Prime - 3229
+ *
+ * File Description:
+ * Contains the code for arcade drive and talons.
+ *
+ * Connect to robot: roborio-3229-FRC.local
+ */
+
+#include <DriveSystem.h>
+
+DriveSystem::DriveSystem()
+{
+	//Initialize the gyro sensitivity
+	gyro.SetSensitivity(GYRO_GAIN);
+
+	//Instantiate motor controllers
+	leftLead = new WPI_TalonSRX(LEFT_LEAD_ID);
+	rightLead = new WPI_TalonSRX(RIGHT_LEAD_ID);
+	leftFollower = new WPI_TalonSRX(LEFT_FOLLOWER_ID);
+	rightFollower = new WPI_TalonSRX(RIGHT_FOLLOWER_ID);
+
+	//Reset the controllers
+	leftLead->Set(ControlMode::PercentOutput, 0);
+	rightLead->Set(ControlMode::PercentOutput, 0);
+	leftFollower->Set(ControlMode::PercentOutput, 0);
+	leftFollower->Set(ControlMode::PercentOutput, 0);
+
+	//Set followers
+	leftFollower->Follow(*leftLead);
+	rightFollower->Follow(*rightLead);
+
+	//Instantiate DriveTrain
+	diffDrive = new frc::DifferentialDrive(*leftLead, *rightLead);
+
+	//Implement drive train safety
+	diffDrive->SetExpiration(SAFETY_TIMEOUT); //Set safety
+	diffDrive->SetMaxOutput(MAX_OUTPUT);
+
+}
+
+DriveSystem::~DriveSystem()
+{
+	delete leftLead;
+	delete rightLead;
+	delete leftFollower;
+	delete rightFollower;
+	delete diffDrive;
+}
+
+void DriveSystem::ResetHeading()
+{
+	//Reset the gyro so zero angle is straight ahead
+	gyro.Reset();
+}
+
+void DriveSystem::Stop()
+{
+	diffDrive->ArcadeDrive(0, 0);
+	frc::Wait(0.05);
+}
+
+
+void DriveSystem::Drive (double& Y, double& X)
+{
+	//Apply smoothing curve to acceleration
+	Y = (pow(MAX_POWER, Y) * Y);
+
+	//Flip the Y value because of the RobotDrive.Drive function
+	//is opposite of the XBoxController
+	Y = -Y;
+
+	//Enforce a max power/speed
+	if(abs(Y) > MAX_POWER) //If the absolute value of y is greater than the max power we are allowing the Sparks to have (0.8)...
+	{
+		if (Y < 0) //Test to see if Y is negative (moving forwards, first two quadrants)
+		{
+			Y = 0 - MAX_POWER; //Make sure y is less than the max power and that it is negative (we are moving forward)
+		}
+		else //Otherwise we are moving backwards (third and fourth quadrants)
+		{
+			Y = MAX_POWER; //Make sure that y is less than the max power, postive because we are moving backwards.
+		}
+	}
+
+	if(abs(X) > MAX_POWER) //Make sure the absolute value of x is not greater than the max power
+	{
+		if (X < 0) //If we are moving left (second and third quadrants).
+		{
+			X = 0 - MAX_POWER; //Make sure x is at the max power and that it is negative (we are moving left)
+		}
+		else //If we are moving right (first and fourth quadrants)
+		{
+			X = MAX_POWER; //Make sure that x is at the max power and that it is positive (we are moving right)
+		}
+	}
+
+	diffDrive->ArcadeDrive(Y, X);
+}
+
+//Drives Straight only in autonomous
+void DriveSystem::DriveStraight()
+{
+}
+
+//Turns the specified angle (in positive of negative degrees from zero) only in autonomous.
+void DriveSystem::DriveTurn (const int& angle)
+{
+}
